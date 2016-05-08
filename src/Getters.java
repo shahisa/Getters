1a3bd930212cfeffa611e8275913608962a97bdd
Isaiah Weaver <iswea1@morgan.edu>
2016-05-08 02:36:25 -0400

    used getters in person class

---
+++ src/Getters.java
@@ -0,0 +1,42 @@
+//creating a new class
+class Person{
+	String name;
+	int age;
+	void speak(){
+		System.out.println("My name is "+ name);
+	}
+	
+	int calculateYearsToRetirement(){
+		int yearsLeft = 65 - age;
+		
+		return yearsLeft;
+	}
+	//Getter methods
+	int getAge(){
+		return age;
+	}
+	String getName(){
+		return name;
+	}
+}
+public class Getters {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+		person1.name = "Yaya";
+		person1.age = 34;
+		// person1.speak();
+		
+		int years = person1.calculateYearsToRetirement();
+		
+		System.out.println("Years till retirement " + years);
+		
+		int age = person1.getAge();
+		String name = person1.getName();
+		
+		System.out.println("Name is "+ name);
+		System.out.println("Age is "+ age);
+	}
+
+}
